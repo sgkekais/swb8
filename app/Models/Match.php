5cdb7bbc86c110f4518bc6dffb8ac3f355b5946b
kvnka <kvnkaiser@gmail.com>
2020-11-15 20:43:09 +0100

    initial commit

---
+++ app/Models/Match.php
@@ -0,0 +1,57 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+use Spatie\Activitylog\Traits\LogsActivity;
+
+class Match extends Model
+{
+    use HasFactory;
+    use LogsActivity;
+
+    /**
+     * The table associated with the model.
+     *
+     * @var string
+     */
+    protected $table = 'matches';
+
+    /**
+     * The attributes that are mass assignable.
+     *
+     * @var array
+     */
+    protected $fillable = [
+        'name', 'name_short', 'name_code', 'logo_url', 'owner', 'ah'
+    ];
+
+    /**
+     * The model's default values for attributes.
+     *
+     * @var array
+     */
+    protected $attributes = [
+        'owner' => false,
+        'ah' => false
+    ];
+
+    /**
+     * The attributes that should be cast to native types.
+     *
+     * @var array
+     */
+    protected $casts = [
+        'owner' => 'boolean',
+        'ah' => 'boolean'
+    ];
+
+    /**
+     * The attributes that should be logged.
+     *
+     * @var array
+     */
+    protected static $logFillable = true;
+    protected static $logOnlyDirty = true;
+}
